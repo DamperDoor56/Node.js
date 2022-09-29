0a7c3a02f02ad0ba70bf738664cccbd50bbd6440
Ludmi Lopez <ludmit@outlook.com>
2022-09-29 19:05:17 -0300

    little chabge

--- router/rutitas.js
+++ router/rutitas.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render("index", {titulo: "mi titulo dinamico"})
-})
-
 router.get('/servicios', (req, res) => {
     res.render("servicios", {tituloServicios: "Este es un mensaje dinamico de servicios"})
 })
