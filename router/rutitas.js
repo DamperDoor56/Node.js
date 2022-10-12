e3a1cdad790fd3b1c73a8147b9b9d8a3fbb6154e
Ludmi Lopez <ludmit@outlook.com>
2022-10-12 20:12:45 -0300

    deleting useless

--- router/rutitas.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/servicios', (req, res) => { //Esta ruta lleva a la pagina de servicio
-    res.render("servicios", {tituloServicios: "Este es un mensaje dinamico de servicios"})
-})
-
-
-module.exports = router;
