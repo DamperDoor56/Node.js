8917b3e407bc641d990c06c3187f261af786027d
Ludmi Lopez <ludmit@outlook.com>
2022-10-17 18:13:43 -0300

    adding timestamps

--- models/Publicacion.js
+++ models/Publicacion.js
@@ -3,9 +3,13 @@ const Schema = mongoose.Schema;
 
 const publiSchema = new Schema({ // El schema de mongoose 
   titulo:  String,
-  descripcion: String
+  descripcion: String,
   // user: String
-});
+},{
+  timestamps: {
+  createdAt: 'created_at', // Use `created_at` to store the created date
+  updatedAt: 'updated_at' // and `updated_at` to store the last updated date
+}});
 
 // Crear el modelo
 const publicacion = mongoose.model('publicacion', publiSchema);
